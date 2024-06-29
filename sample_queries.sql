410fd82247aeb69ff98b4fd08a76c41f678c972a
prajul-shinde <prajulshinde@gmail.com>
2024-06-29 14:16:22 +0530

    summerizing data

--- sample_queries.sql
+++ sample_queries.sql
@@ -121,3 +121,42 @@ SELECT customer_id FROM customers WHERE points > 3000);
 -- DELETE --
 USE sql_invoicing;
 DELETE FROM invoices where client_id=(SELECT client_id FROM clients where name ='Myworks');
+
+------- ############ 4. Summerizing data ########## --
+-- min
+-- max 
+-- avg
+-- sum 
+-- count
+
+SELECT 'First half of 2019' AS date_range,
+SUM(invoice_total) AS total_sales,
+SUM(payment_total) AS total_payments,
+SUM(invoice_total-payment_total) AS 'What we expect'
+FROM invoices WHERE invoice_date BETWEEN '2019-01-01' AND '2019-06-30'
+UNION
+SELECT 'Second half of 2019' AS date_range,
+SUM(invoice_total) AS total_sales,
+SUM(payment_total) AS total_payments,
+SUM(invoice_total-payment_total) AS 'What we expect'
+FROM invoices WHERE invoice_date BETWEEN '2019-07-01' AND '2019-12-31'
+UNION 
+SELECT 'TOTAL' AS date_range,
+SUM(invoice_total) AS total_sales,
+SUM(payment_total) AS total_payments,
+SUM(invoice_total-payment_total) AS 'What we expect'
+FROM invoices WHERE invoice_date BETWEEN '2019-01-01' AND '2019-12-31';
+
+-- GRoup by --
+SELECT p.date, pm.name AS payment_method, SUM(amount) AS total_payment 
+FROM payments p JOIN payment_methods pm ON p.payment_method= pm.payment_method_id
+GROUP BY date, payment_method
+ORDER BY p.date;
+
+-- having clause --
+SELECT c.customer_id, c.first_name, c.last_name, SUM(oi.quantity*oi.unit_price) AS total_sales FROM customers c
+JOIN orders o USING(customer_id)
+JOIN order_items oi USING(order_id)
+WHERE state='VA'
+GROUP BY c.customer_id, c.first_name, c.last_name
+HAVING total_sales>100;
